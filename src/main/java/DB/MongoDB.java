1775718c5f4eae048cbc0f0dd3d6392c758b84f2
bartek <klys.bartosz@gmail.com>
2016-06-16 15:42:01 +0200

    Added javadocs for two methods.

---
+++ src/main/java/DB/MongoDB.java
@@ -0,0 +1,37 @@
+package DB;
+
+import com.mongodb.*;
+import com.mongodb.util.JSON;
+
+public class MongoDB {
+
+    public static void main(String[] args) {
+
+        Mongo mongo = new Mongo("localhost", 27017);
+        DB db = mongo.getDB("testdb");
+        DBCollection document = db.getCollection("employees"); // name of MongoDB document
+        String json = "{\"name\" : \"Bartek Klys\"}"; //json we want to insert to document
+
+        //insertJSON(json, document);
+        document.drop();
+        find(document);
+    }
+
+    /**
+     * This method prints all records from database.
+     * @param channelDBCollection - name of MongoDB document.
+     */
+    private static void find (DBCollection channelDBCollection){
+        DBCursor dbCursor = channelDBCollection.find();
+        while (dbCursor.hasNext()) System.out.println(dbCursor.next());
+    }
+
+    /**
+     * This method inserts json into a document in database.
+     * @param json - this is JSON we want to insert into a document.
+     * @param document - document in out database.
+     */
+    private static void insertJSON(String json, DBCollection document){
+        document.insert((DBObject) JSON.parse(json));
+    }
+}
