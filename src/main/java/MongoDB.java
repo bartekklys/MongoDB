f64528bfd94748f99dab15d8d96558139848fcea
bartek <klys.bartosz@gmail.com>
2016-06-16 14:57:10 +0200

    Added insert function.

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -1,4 +1,5 @@
 import com.mongodb.*;
+import com.mongodb.util.JSON;
 
 public class MongoDB {
 
@@ -7,14 +8,20 @@ public class MongoDB {
         Mongo mongo = new Mongo("localhost", 27017);
         DB db = mongo.getDB("testdb");
         DBCollection table = db.getCollection("testdb");
+        String json = "{\"name\" : \"Bartus Klys\"}";
 
         find(table);
-
+        insertJSON(json, table);
+        find(table);
 
     }
 
     private static void find (DBCollection channelDBCollection){
         DBCursor dbCursor = channelDBCollection.find();
         while (dbCursor.hasNext()) System.out.println(dbCursor.next());
+    }
+
+    private static void insertJSON(String command, DBCollection dbCollection){
+        dbCollection.insert((DBObject) JSON.parse(command));
     }
 }
